57c7ed24ae6c22017feccab71cdf0ce50692e1a0
Marco Marranchelli <marco.marranchelli@me.com>
2018-06-20 13:26:20 +0200

    note model und mongo model angepasst

--- src/frontend/js/model/note.js
+++ src/frontend/js/model/note.js
@@ -13,23 +13,10 @@ class Note {
         this.priority = priority;
         this.dueDate = dueDate;
         this.completed = true;
-        this.createDate = Date.now();
-        this.editedDate = Date.now();
+        this.completedAt = null;
+        this.createdAt = Date.now();
+        this.editedAt = Date.now();
     }
-
-    toJSON() {
-        return {
-            id: this.id,
-            title: this.title,
-            description: this.description,
-            priority: this.priority,
-            dueDate: this.dueDate,
-            completed: this.completed,
-            createDate: this.createDate,
-            editedDate: this.editedDate
-        };
-    }
-
 }
 
 export default Note;
